d6f8c1eb2f75fb9705048096ce9ed3ed3cddf310
Alexander Jank <alex@zeraton.de>
2020-12-15 11:33:31 +0100

    style(lint): add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'prettier'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  // add your custom rules here
+  rules: {}
+};
